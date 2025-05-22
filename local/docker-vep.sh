10891339e3936c88ccbf24f5f0f51ce4e5df7d3b
Anish S. Shah <asshah4@users.noreply.github.com>
2025-05-21 19:44:33 -0600

    add docker command for vep

--- local/docker-vep.sh
+++ local/docker-vep.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# This generally works for LoF, assuming that Loftee is in the correct place
+# Dockerized version works well as long as files are mounted appropriately
 docker run \
 	-v $HOME/.vep:/data \
-	-v $HOME/data/genetics/uic_second_batch/raw:/input \
-	-v $HOME/data/genetics/uic_second_batch/vep:/output \
+	-v $HOME/data/genetics/uic_first_batch/vcf:/input \
+	-v $HOME/data/genetics/uic_first_batch/tmp:/output \
+	-v $HOME/.vep/loftee:/plugins \
 	ensemblorg/ensembl-vep vep \
-	-i /input/SM-OJT8K.hard-filtered.vcf \
-	-o /output/tmp_vep.txt \
-	--cache --offline --verbose \
-	--polyphen s --sift s --symbol --show_ref_allele \
-	--plugin LoF,loftee_path:$HOME/tools/loftee,human_ancestor_fa:false \
-	--dir_plugin $HOME/tools/loftee/
+	--input_file /input/CCDG_Broad_CVD_AF_Darbar_UIC_Cases-UIC0330.vcf \
+	--output_file /output/tmp_vep.txt \
+	--format vcf \
+	--cache \
+	--force_overwrite \
+	--plugin LoF,loftee_path:/plugins,human_ancestor_fa:false 
 
 

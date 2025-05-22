c7b5f71dcc31ebb10716e86b68c6f9763b32a2d1
Anish S. Shah <asshah4@users.noreply.github.com>
2025-05-21 20:26:37 -0600

    update

--- local/docker-vep.sh
+++ local/docker-vep.sh
@@ -4,15 +4,17 @@
 # Dockerized version works well as long as files are mounted appropriately
 docker run \
 	-v $HOME/.vep:/data \
-	-v $HOME/data/genetics/uic_first_batch/vcf:/input \
-	-v $HOME/data/genetics/uic_first_batch/tmp:/output \
+	-v $HOME/data/genetics/uic_second_batch/vcf:/input \
+	-v $HOME/data/genetics/uic_second_batch/vep:/output \
 	-v $HOME/.vep/loftee:/plugins \
 	ensemblorg/ensembl-vep vep \
-	--input_file /input/CCDG_Broad_CVD_AF_Darbar_UIC_Cases-UIC0330.vcf \
+	--input_file /input/SM-OJT8K.hard-filtered.vcf \
 	--output_file /output/tmp_vep.txt \
 	--format vcf \
 	--cache \
 	--force_overwrite \
+	--show_ref_allele \
+	--everything \
 	--plugin LoF,loftee_path:/plugins,human_ancestor_fa:false 
 
 

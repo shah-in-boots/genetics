a1a906101d2af1c1b2036ce95af332e350b024c1
Anish S. Shah <asshah4@users.noreply.github.com>
2025-11-26 15:57:07 -0700

    add submit file

---
+++ cluster/submit-convert-vep-to-table.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=vep_table
+#SBATCH --partition=batch
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32GB
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/vep_table_%A_%a.out
+#SBATCH --error=logs/vep_table_%A_%a.err
+
+# Script for submission 
+# Used like this`sbatch submit-convert-vep-to-table.sh "${BATCH_DIR}"``
+
+# Take arguments from command line
+BATCH_DIR="$1"
+
+# Make sure path for libraries is present
+export R_LIBS_USER="~/tools/R/library"
+
+# Load R module
+module load R/4.4.1-gfbf-2023b
+
+# Run script
+Rscript convert-vep-to-table.R "${BATCH_DIR}"
